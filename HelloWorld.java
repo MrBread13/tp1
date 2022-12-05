1e2781a0c2c2ea589a7c1a90f73ac8217c06e4c5
Valentin Bonsi <valentin.bonsi@eazimuth.fr>
2022-12-05 15:53:40 +0100

    translated HellowWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld {
 	public static void main (String[] args) {
-	System.out.println("Hello World !");
+	System.out.println("Bonjour Monde!");
 	}
 }
-System.out.println("Goodbye !");
+System.out.println("Au revoir !");
